84ab9da9c6f8a441c88709a0998cbd474c157613
drchubbles <114992476+drchubbles@users.noreply.github.com>
2023-07-18 22:05:00 +0100

    Add files via upload

---
+++ 441101-2223-capstone-project-drchubbles/Capstone-Project-441101-2223/Capstone-Project-441101-2223/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Capstone_Project_441101_2223;
+using static Capstone_Project_441101_2223.ProjectManager;
+
+Console.WriteLine("Capstone Project Menu");
+
+ProjectManager manager = new ProjectManager();
+
+new ProjectManagerMenu(manager).Select();
